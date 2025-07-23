45cf994c96b0dd6407167b7321d001d72955165c
Rodrigo Santiago <33rodrigosantiago@gmail.com>
2025-07-22 21:42:14 -0300

    Criando o repositório de pessoas

---
+++ src/models/sqlite/repositories/people_repository.py
@@ -0,0 +1,47 @@
+from src.models.sqlite.entities.people import PeopleTable
+from src.models.sqlite.entities.pets import PetsTable
+from sqlalchemy.orm.exc import NoResultFound
+
+class PeopleRepository:
+    def __init__(self, db_connection) -> None:
+        self.__db_connection = db_connection
+    
+    def get_person(self, person_id: int):
+        with self.__db_connection as database:
+            try:
+                person = (
+                    database.session
+                        .query(PeopleTable)
+                        .outerjoin(PetsTable, PetsTable.id == PeopleTable.pet_id)
+                        .filter(PeopleTable.id == person_id)
+                        .with_entities(
+                            PeopleTable.first_name,
+                            PeopleTable.last_name,
+                            PetsTable.name.label("pet_name"),
+                            PetsTable.type.label("pet_type"),
+                        )
+                        .one()
+                )
+
+                return person
+            
+            except NoResultFound as exception:
+                return None
+    
+    def insert_person(self, first_name: str, last_name: str, age: int, pet_id: int):
+        with self.__db_connection as database:
+            try:
+                person_data = PeopleTable(
+                    first_name=first_name,
+                    last_name=last_name,
+                    age=age,
+                    pet_id=pet_id
+                )
+
+                database.session.add(person_data)
+                database.session.commit()
+
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+            
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,8 +1,9 @@
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 import pytest
 
-# db_connection_handler.connect_to_db()
+db_connection_handler.connect_to_db()
 
 @pytest.mark.skip(reason="interacao com o banco de dados")
 def test_list_pets():
@@ -17,4 +18,21 @@ def test_delete_pets():
     repo.delete_pets(name="belinha")
     response = repo.list_pets()
     print(response)
+
+
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_list_pets():
+    repo = PeopleRepository(db_connection=db_connection_handler)
+    response = repo.get_person(person_id=1)
+    print(response)
+
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_insert_people():
+    repo = PeopleRepository(db_connection=db_connection_handler)
+    repo.insert_person(
+        first_name="Rodrigo",
+        last_name="Santiago",
+        age=26,
+        pet_id=1
+    )
     
